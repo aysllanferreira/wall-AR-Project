14954d4bf40fcb432574708d38e19ecd92b1f8b4
aysllanferreira <aysllan.ferreiraks@gmail.com>
2023-03-11 13:01:49 -0300

    :sparkles: feat: new code!

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
+// Create video element for camera feed
+const video = document.createElement('video')
+video.setAttribute('autoplay', '')
+video.setAttribute('playsinline', '')
+document.body.appendChild(video)
+
 // Get video feed from webcam
-const video = document.getElementById('video')
 navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
   video.srcObject = stream
   video.play()
 })
 
-// Set up AR.js
-const canvas = document.getElementById('canvas')
-canvas.style.width = window.innerWidth + 'px'
-canvas.style.height = window.innerHeight + 'px'
-const renderer = new THREE.WebGLRenderer({ canvas })
-renderer.setSize(window.innerWidth, window.innerHeight)
-const arToolkitSource = new THREEx.ArToolkitSource({ sourceElement: video, width: window.innerWidth, height: window.innerHeight })
-arToolkitSource.init(() => {
-  setTimeout(() => arToolkitSource.onResize(renderer.domElement), 1)
-})
-const arToolkitContext = new THREEx.ArToolkitContext({ cameraParametersUrl: './camera_para.dat', detectionMode: 'mono' })
-arToolkitContext.init(() => {
-  camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix())
-})
-const markerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, { type: 'pattern', patternUrl: './pattern-teste (1).patt', changeMatrixMode: 'cameraTransformMatrix' })
-
-// Create and position image on marker
-const loader = new THREE.TextureLoader()
-loader.load('./teste.jpeg', (texture) => {
-  const geometry = new THREE.PlaneGeometry(1, 1)
-  const material = new THREE.MeshBasicMaterial({ map: texture })
-  const mesh = new THREE.Mesh(geometry, material)
-  mesh.position.set(0, 0.5, 0) // Position the image on the marker
-  markerRoot.add(mesh)
-})
-
-// Render
-const scene = new THREE.Scene()
-const camera = new THREE.Camera()
-const markerRoot = new THREE.Group()
-markerRoot.add(camera)
-scene.add(markerRoot)
-function render () {
-  requestAnimationFrame(render)
-  if (arToolkitSource.ready) {
-    arToolkitContext.update(arToolkitSource.domElement)
-    scene.visible = camera.visible
+AFRAME.registerComponent('image-loader', {
+  init: function () {
+    this.el.addEventListener('loaded', () => {
+      this.el.object3D.visible = false
+    })
   }
-  renderer.render(scene, camera)
-}
-render()
+})
+
+AFRAME.registerComponent('location-based-ar', {
+  init: function () {
+    navigator.geolocation.watchPosition((position) => {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
+      const altitude = position.coords.altitude || 0
+
+      const marker = document.querySelector('a-marker')
+      const markerPosition = new THREE.Vector3(-21.2665809, altitude, -48.5037126)
+      const worldPosition = ARjs.Helpers.getMarkerWorldPosition(marker.object3D, markerPosition)
+      marker.object3D.position.set(worldPosition.x, worldPosition.y, worldPosition.z)
+
+      const distance = ARjs.Helpers.distanceBetweenPoints(markerPosition, new THREE.Vector3(latitude, altitude, -longitude))
+      const maxDistance = 0.05
+      const image = document.querySelector('#my-image')
+
+      if (distance <= maxDistance) {
+        image.setAttribute('visible', 'true')
+      } else {
+        image.setAttribute('visible', 'false')
+      }
+    })
+  }
+})
